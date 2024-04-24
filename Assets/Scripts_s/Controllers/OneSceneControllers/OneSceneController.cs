d4ead1b83f671e0026b95697be9e1df4bc74a113
daria <dzinkina@bk.ru>
2024-04-25 00:44:30 +0500

    Ending 0-25

---
+++ Assets/Scripts_s/Controllers/OneSceneControllers/OneSceneController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OneSceneController : MonoBehaviour
+{
+    public ChangerScene currScene;
+    public BottomBarController bar;
+    public ChooseControllerOne chooseController;
+
+    private State state = State.IDLE;
+
+    private enum State
+    {
+        IDLE, ANIMATE, CHOOSE
+    }
+
+    void Start()
+    {
+        if (currScene is StoryScene)
+        {
+            StoryScene storyScene = currScene as StoryScene;
+            bar.PlayScene(storyScene);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (state == State.IDLE && bar.IsCompleted())
+            {
+                if (bar.IsLastSentence())
+                {
+                    StoryScene story = currScene as StoryScene;
+                    if (story.IsLastScene)
+                    {
+                        LoadNextScene();
+                    }
+                    else
+                    {
+                        PlayScene(story.nextScene);
+                    }
+                }
+                else
+                {
+                    bar.PlaySentence();
+                }
+            }
+        }
+    }
+
+    public void LoadNextScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    public void PlayScene(ChangerScene scene)
+    {
+        StartCoroutine(SwitchScene(scene));
+    }
+
+    private IEnumerator SwitchScene(ChangerScene scene)
+    {
+        state = State.ANIMATE;
+        currScene = scene;
+        bar.Hide();
+        yield return new WaitForSeconds(1f);
+        if (scene is StoryScene)
+        {
+            StoryScene storyScene = scene as StoryScene;
+            yield return new WaitForSeconds(1f);
+            bar.ClearBar();
+            bar.Show();
+            yield return new WaitForSeconds(1f);
+            bar.PlayScene(storyScene);
+            state = State.IDLE;
+        }
+        else if (scene is ChooseScene)
+        {
+            state = State.CHOOSE;
+            chooseController.SetupChoose(scene as ChooseScene);
+        }
+    }
+}
---
+++ Assets/Scripts_s/Inventory/Get_item.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        if (PlayerPrefs.GetInt(gameObject.name) == 1) Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (PlayerPrefs.GetInt(gameObject.name) != 1)
+            {
+                var items = PlayerPrefs.GetString("items_list") + gameObject.name + "\n";
+                PlayerPrefs.SetString("items_list", items);
+                PlayerPrefs.SetInt(gameObject.name, 1);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
