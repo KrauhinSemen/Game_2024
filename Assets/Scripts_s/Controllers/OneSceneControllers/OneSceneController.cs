3915d69aa100afa3ac6325c75cad02f5b211129d
daria <dzinkina@bk.ru>
2024-04-30 10:51:30 +0500

    Novel part with Kustova

--- Assets/Scripts_s/Controllers/OneSceneControllers/OneSceneController.cs
+++ Assets/Scripts_s/Controllers/OneSceneControllers/OneSceneController.cs
@@ -52,7 +52,7 @@ public class OneSceneController : MonoBehaviour
         }
     }
 
-public void LoadNextScene()
+    public void LoadNextScene()
     {
         if (SceneManager.GetActiveScene().buildIndex == 1)
         {
